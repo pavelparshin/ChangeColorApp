88f2da534878142059aa732b382c504400f3064f
Pavel Parshin <mail@pavelparshin.ru>
2020-05-23 00:22:56 +0300

    add func setColorWithRGB() to set value GRB sliders in colorView box

--- ChangeColorApp/ViewController.swift
+++ ChangeColorApp/ViewController.swift
@@ -25,22 +25,47 @@ class ViewController: UIViewController {
         
         colorViewForm.layer.cornerRadius = 10
         
+        //View default Slider's values
+        redValueLabel.text = setValueLabel(for: redValueSlider.value)
+        greenValueLabel.text = setValueLabel(for: greenValueSlider.value)
+        blueValueLabel.text = setValueLabel(for: blueValueSlider.value)
+        
+        //Slider's minimum color
         redValueSlider.minimumTrackTintColor = .red
         greenValueSlider.minimumTrackTintColor = .green
         blueValueSlider.minimumTrackTintColor = .blue
         
+        colorViewForm.backgroundColor = setColorWithRGB()
     }
     
+    
     @IBAction func setRedValueSlider() {
-        redValueLabel.text = String(format: "%.2f", redValueSlider.value)
+        redValueLabel.text = setValueLabel(for: redValueSlider.value)
+        colorViewForm.backgroundColor = setColorWithRGB()
     }
     
     @IBAction func setGreenValueSlider() {
-        greenValueLabel.text = String(format: "%.2f", greenValueSlider.value)
+        greenValueLabel.text = setValueLabel(for: greenValueSlider.value)
+        colorViewForm.backgroundColor = setColorWithRGB()
     }
     
     @IBAction func setBlueValueSlider() {
-        blueValueLabel.text = String(format: "%.2f", blueValueSlider.value)
+        blueValueLabel.text = setValueLabel(for: blueValueSlider.value)
+        colorViewForm.backgroundColor = setColorWithRGB()
+    }
+    
+    
+    //Determine current settings RGB sliders and return UIColor
+    private func setColorWithRGB() -> UIColor {
+        return UIColor(red: CGFloat(redValueSlider.value),
+        green: CGFloat(greenValueSlider.value),
+        blue: CGFloat(blueValueSlider.value),
+        alpha: 1)
+    }
+    
+    //Reduce value of slider
+    private func setValueLabel(for value: Float) -> String {
+        return String(format: "%.2f", value)
     }
     
 }
