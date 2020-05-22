3f6f25accb9323618caaf9781b619444aeb0b215
Pavel Parshin <mail@pavelparshin.ru>
2020-05-22 19:33:31 +0300

    Create main property for View, Labels and Sliders

--- ChangeColorApp/ViewController.swift
+++ ChangeColorApp/ViewController.swift
@@ -10,11 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var colorViewForm: UIView!
+    
+    @IBOutlet var redValueLabel: UILabel!
+    @IBOutlet var greenValueLabel: UILabel!
+    @IBOutlet var blueValueLabel: UILabel!
+    
+    @IBOutlet var redValueSlider: UISlider!
+    @IBOutlet var greenValueSlider: UISlider!
+    @IBOutlet var blueValueSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        colorViewForm.layer.cornerRadius = 10
+        
+        redValueSlider.minimumTrackTintColor = .red
+        greenValueSlider.minimumTrackTintColor = .green
+        blueValueSlider.minimumTrackTintColor = .blue
+        
     }
-
-
+    
+    @IBAction func setRedValueSlider() {
+    }
+    
+    @IBAction func setGreenValueSlider() {
+    }
+    
+    @IBAction func setBlueValueSlider() {
+    }
+    
 }
 
